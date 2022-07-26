99123dfa3fedab66a43511e67ec1e904f18e4cc7
kuhmasii <olaisaiah54@gmail.com>
2022-07-26 22:23:22 +0100

    feat: edu functionality

--- climateHackton/api/urls.py
+++ climateHackton/api/urls.py
@@ -7,12 +7,27 @@ from account.views import (
     profile_detail_update_delete
 )
 
+from edu.views import (
+    climateEducation_list,
+    climateFact_list,
+    climateEducation_detail_update_delete,
+    climateFact_detail_update_delete,
+)
+
 urlpatterns = [
     path('user/', user_list_create, name='user-list-create'),
     path('user/delete/account/', user_delete, name='user-delete'),
     path('author/', profile_list, name='profile-lsit'),
     path('author/<str:pk>/', profile_detail_update_delete,
          name='profile_detail_update_delete'),
+
+    #  edu
+    path('climate/', climateEducation_list, name='climate-list'),
+    path('climate/<int:pk>/', climateEducation_detail_update_delete,
+         name='climate-detail-update-delete'),
+    path('climate/fact/', climateFact_list, name='climate-fact-list'),
+    path('climate/fact/<int:pk>/', climateFact_detail_update_delete,
+         name='climate-fact-detail-update-delete'),
 
     path('login/', obtain_auth_token)
 
