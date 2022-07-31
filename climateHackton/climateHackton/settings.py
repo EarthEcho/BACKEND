7ef826f3462263e190d77751fd2d22e1b0e430f4
kuhmasii <olaisaiah54@gmail.com>
2022-07-31 11:28:27 +0100

    preping for production

--- climateHackton/climateHackton/settings.py
+++ climateHackton/climateHackton/settings.py
@@ -1,18 +1,22 @@
+import environ
 import django_heroku
 from pathlib import Path
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k4do0kan2+18v9i^-@v&(*c708tr(08o+45=k0!ti8!gfn*$#4'
+SECRET_KEY = 'x6*=whs05%@%ru)cclqh=t)5ltxc#9_imddhb7ah_piox7phd-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
